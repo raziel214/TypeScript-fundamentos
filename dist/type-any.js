83540c10076db3cad2abfe3a63de5ca1c2b9a9c2
raziel214 <soulreavers214@gmail.com>
2022-05-30 13:14:53 -0500

    tipos de variables undefined en TypeScript

---
+++ dist/type-any.js
@@ -0,0 +1,16 @@
+"use strict";
+// Tipo explicito
+let idUser;
+idUser = 1; // number
+idUser = '1¿hello'; // string
+console.log('idUser', idUser);
+// Tipo Inferido
+let otherId;
+otherId = 1;
+otherId = '1';
+// otherId = true;
+console.log('otherId', otherId);
+let surprise = 'hello typescript';
+// surprise.sayHello(); // Error
+const res = surprise.substring(6); // Error
+console.log('res', res);
