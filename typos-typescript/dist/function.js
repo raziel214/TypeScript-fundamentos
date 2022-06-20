d6fdc9a1b9e1c8dc0ddda9d90ecfc6d0de48f0c3
raziel214 <raziel214@users.noreply.github.com>
2022-06-20 17:25:55 -0500

    Ajuste de herencias

---
+++ typos-typescript/dist/function.js
@@ -0,0 +1,49 @@
+"use strict";
+// Crear una Fotografia
+// function createPicture(title, date, size) {
+// //  title 
+// }
+// Usamos TS, definimos tipos para parametros
+// function createPicture(title: string, date: string, size: SquareSize) {
+//     // Se crea la Fotografia
+//     console.log('create Picture using', title, date, size);
+// }
+// createPicture('My Birthday', '2020-03-10', '500x500');
+// createPicture('Colombia Trip', '2020-03');
+// Parametros Opcionales en funciones
+function createPicture(title, date, size) {
+    // Se crea la Fotografia
+    console.log('create Picture using', title, date, size);
+}
+createPicture('My Birthday', '2020-03-10', '500x500');
+createPicture('Colombia Trip', '2020-03');
+createPicture('Colombia Trip');
+createPicture();
+// Flat Array Function
+let createPic = (title, date, size) => {
+    // return {
+    //     title: title,
+    //     date: date,
+    //     size: size
+    // };
+    return { title, date, size };
+};
+const picture = createPic('Platzi session', '2020-03-10', '100x100');
+console.log('picture', picture);
+function handleError2(code, message) {
+    // Procesamiento del codigo, mensaje
+    if (message === 'error') {
+        throw new Error(`${message}. Code error: ${code}`);
+    }
+    else {
+        return 'An error has occurred';
+    }
+}
+try {
+    let result = handleError2(200, 'OK'); // string
+    console.log('result', result);
+    result = handleError2(404, 'error'); // never
+    console.log('result', result);
+}
+catch (error) {
+}
